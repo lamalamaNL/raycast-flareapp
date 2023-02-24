fc96a0aeeecae0185a70945fd883cc96968a71f8
Mark de Vries <mark@lamalama.nl>
2023-02-24 14:00:31 +0100

    finalyze projects

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,64 @@
-import { ActionPanel, List, Action, getPreferenceValues } from "@raycast/api";
+import { ActionPanel, List, Action, getPreferenceValues, showToast, Toast } from "@raycast/api";
 import { useEffect, useState } from "react";
 import fetch from "node-fetch";
 import { FLAREAPP_API_URL } from "./config";
 
-interface Project {
+export default function Command() {
+  const [error, setError] = useState<Error>();
+  const [projects, setProjects] = useState<IProject[]>([]);
+  const [loading, setLoading] = useState(true);
+  const { API_TOKEN } = getPreferenceValues();
+
+  useEffect(() => {
+    async function getProjects() {
+      const response = await fetch(`${FLAREAPP_API_URL}/projects?api_token=${API_TOKEN}`, {
+        headers: { Accept: "application/json" },
+      });
+
+      let data: { data: IProject[] } = { data: [] };
+      try {
+        data = (await response.json()) as ProjectsResponse;
+      } catch (e) {
+        setError(new Error("while fetching your projects"));
+      } finally {
+        setProjects(data.data);
+        setLoading(false);
+      }
+    }
+
+    getProjects();
+  }, []);
+
+  useEffect(() => {
+    if (error) {
+      showToast({
+        style: Toast.Style.Failure,
+        title: "Something went wrong",
+        message: error.message,
+      });
+    }
+  }, [error]);
+
+  return (
+    <List isLoading={loading}>
+      {projects.map((project) => {
+        return (
+          <List.Item
+            key={project.id}
+            title={project.name}
+            actions={
+              <ActionPanel title="Flare">
+                <Action.OpenInBrowser url={`https://flareapp.io/projects/${project.id}`} />
+              </ActionPanel>
+            }
+          />
+        );
+      })}
+    </List>
+  );
+}
+
+interface IProject {
   id: number;
   name: string;
   errors_last_30_days_count: number;
@@ -15,53 +70,6 @@ interface Project {
   stage: string;
 }
 
-interface ProjectsResponse {
-  data: Project[];
-}
-
-export default function Command() {
-  const [projects, setProjects] = useState<Project[]>([]);
-  const [loading, setLoading] = useState(true);
-  const { API_TOKEN } = getPreferenceValues();
-
-  useEffect(() => {
-    async function getProjects() {
-      const response = await fetch(`${FLAREAPP_API_URL}/projects?api_token=${API_TOKEN}`, {
-        headers: {
-          Accept: "application/json",
-        },
-      });
-
-      let data: { data: Project[] } = { data: [] };
-      try {
-        data = (await response.json()) as ProjectsResponse;
-      } catch (e) {
-        console.error(e);
-      } finally {
-        setProjects(data.data);
-        setLoading(false);
-      }
-    }
-
-    getProjects();
-  }, []);
-
-  return (
-    <List isLoading={loading}>
-      {projects &&
-        projects.map((project) => {
-          return (
-            <List.Item
-              key={project.id}
-              title={project.name}
-              actions={
-                <ActionPanel title="Flare">
-                  <Action.OpenInBrowser url={`https://flareapp.io/projects/${project.id}`} />
-                </ActionPanel>
-              }
-            />
-          );
-        })}
-    </List>
-  );
-}
+type ProjectsResponse = {
+  data: IProject[];
+};
